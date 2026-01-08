9e76b1569b76ed2eeffa45522701511b49951696
ryansyuan <118715861+ryansyuan@users.noreply.github.com>
2026-01-08 20:31:21 +0800

    Update 6.2main.c

--- 6.2main.c
+++ 6.2main.c
@@ -1,44 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _dates{
+    int year, month, day;
+} dates;
+
+typedef struct _order{
+    char num[11];   // è¨‚å–®ç·¨è™Ÿ
+    dates date;      // è¨‚å–®æ—¥æœŸ
+    int amount;     // è¨‚å–®é‡‘é¡
+} Order;
+
 int main(void)
 {
-    typedef struct _dates
-    {
-        int years, month, day;
-    } dates;
-
-    typedef struct _order
-    {
-        char num[11];   // ­q³æ½s¸¹
-        dates date;     // ­q³f¤é´Á
-        int amount;     // ­q³fª÷ÃB
-    } order;
-
-    order ord1 = { "AHB0A1024",{1994, 3, 8}, 1487 };
-    order ord2 = { "US0A81025",{2015, 5, 20}, 54530 };
-    order ord3 = { "UN0BA1028",{2020, 8, 7}, 663613 };
+    Order ord1 = { "AHB0A1024",{1994, 3, 8}, 1487 };
+    Order ord2 = { "US0A81025",{2015, 5, 20}, 54530 };
+    Order ord3 = { "UN0BA1028",{2020, 8, 7}, 663613 };
 
     printf("    ******************************\n");
-    printf("    *        ­q³æ¸ê®Æ            *\n");
+    printf("    *        è¨‚å–®è³‡æ–™            *\n");
     printf("    ******************************\n");
 
     printf("---------------------------------------------------\n");
-    printf("   ­q³æ½s¸¹         ­q³f¤é´Á         ­q³fª÷ÃB\n");
+    printf("   è¨‚å–®ç·¨è™Ÿ         è¨‚å–®æ—¥æœŸ         è¨‚å–®é‡‘é¡\n");
     printf("---------------------------------------------------\n");
 
     printf("%-10s %4d/%02d/%02d   NT$%10d\n",
-        ord1.num, ord1.date.years, ord1.date.month, ord1.date.day, ord1.amount);
+        ord1.num, ord1.date.year, ord1.date.month, ord1.date.day, ord1.amount);
 
     printf("%-10s %4d/%02d/%02d   US$%10d\n",
-        ord2.num, ord2.date.years, ord2.date.month, ord2.date.day, ord2.amount);
+        ord2.num, ord2.date.year, ord2.date.month, ord2.date.day, ord2.amount);
 
     printf("%-10s %4d/%02d/%02d   HK$%10d\n",
-        ord3.num, ord3.date.years, ord3.date.month, ord3.date.day, ord3.amount);
-
-    printf("\n\n");
-
+        ord3.num, ord3.date.year, ord3.date.month, ord3.date.day, ord3.amount);
+    
+    printf("\n\n")
+        
     system("pause");
     return 0;
 }
-
